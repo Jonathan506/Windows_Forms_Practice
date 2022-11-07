fa4f1b8573c76b54889cccfe5ab24b88e23a3ed9
Jonathan <blackfury0399@gmail.com>
2022-11-07 00:08:38 -0600

    Basic Calculator

---
+++ Suma/Suma/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace Suma
+{
+    partial class Suma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsuma = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.Number1 = new System.Windows.Forms.Label();
+            this.Number2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.firstnumber = new System.Windows.Forms.TextBox();
+            this.secondnumber = new System.Windows.Forms.TextBox();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.Respuesta = new System.Windows.Forms.TextBox();
+            this.btnresta = new System.Windows.Forms.Button();
+            this.btmmultiplicacion = new System.Windows.Forms.Button();
+            this.btndivision = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnsuma
+            // 
+            this.btnsuma.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnsuma.FlatAppearance.BorderSize = 0;
+            this.btnsuma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsuma.Location = new System.Drawing.Point(3, 3);
+            this.btnsuma.Name = "btnsuma";
+            this.btnsuma.Size = new System.Drawing.Size(76, 62);
+            this.btnsuma.TabIndex = 0;
+            this.btnsuma.Text = "+";
+            this.btnsuma.UseVisualStyleBackColor = false;
+            this.btnsuma.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(167, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(76, 62);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "C";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Number1
+            // 
+            this.Number1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number1.Location = new System.Drawing.Point(33, 78);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(199, 34);
+            this.Number1.TabIndex = 1;
+            this.Number1.Text = "First Number :";
+            this.Number1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Number2
+            // 
+            this.Number2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number2.Location = new System.Drawing.Point(33, 145);
+            this.Number2.Name = "Number2";
+            this.Number2.Size = new System.Drawing.Size(199, 34);
+            this.Number2.TabIndex = 1;
+            this.Number2.Text = "Second Number :";
+            this.Number2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Location = new System.Drawing.Point(238, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 3);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel2.Location = new System.Drawing.Point(238, 165);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 3);
+            this.panel2.TabIndex = 2;
+            // 
+            // firstnumber
+            // 
+            this.firstnumber.BackColor = System.Drawing.SystemColors.MenuText;
+            this.firstnumber.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.firstnumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstnumber.ForeColor = System.Drawing.SystemColors.Window;
+            this.firstnumber.Location = new System.Drawing.Point(238, 73);
+            this.firstnumber.Name = "firstnumber";
+            this.firstnumber.Size = new System.Drawing.Size(200, 23);
+            this.firstnumber.TabIndex = 3;
+            this.firstnumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // secondnumber
+            // 
+            this.secondnumber.BackColor = System.Drawing.SystemColors.MenuText;
+            this.secondnumber.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.secondnumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondnumber.ForeColor = System.Drawing.SystemColors.Window;
+            this.secondnumber.Location = new System.Drawing.Point(238, 136);
+            this.secondnumber.Name = "secondnumber";
+            this.secondnumber.Size = new System.Drawing.Size(200, 23);
+            this.secondnumber.TabIndex = 3;
+            this.secondnumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.Location = new System.Drawing.Point(137, 247);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(199, 34);
+            this.labelResultado.TabIndex = 1;
+            this.labelResultado.Text = "Resultado :";
+            this.labelResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Respuesta
+            // 
+            this.Respuesta.BackColor = System.Drawing.SystemColors.MenuText;
+            this.Respuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Respuesta.ForeColor = System.Drawing.SystemColors.Window;
+            this.Respuesta.Location = new System.Drawing.Point(327, 250);
+            this.Respuesta.Multiline = true;
+            this.Respuesta.Name = "Respuesta";
+            this.Respuesta.ReadOnly = true;
+            this.Respuesta.Size = new System.Drawing.Size(95, 31);
+            this.Respuesta.TabIndex = 3;
+            this.Respuesta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnresta
+            // 
+            this.btnresta.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnresta.FlatAppearance.BorderSize = 0;
+            this.btnresta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnresta.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnresta.Location = new System.Drawing.Point(85, 3);
+            this.btnresta.Name = "btnresta";
+            this.btnresta.Size = new System.Drawing.Size(76, 62);
+            this.btnresta.TabIndex = 0;
+            this.btnresta.Text = "-";
+            this.btnresta.UseVisualStyleBackColor = false;
+            this.btnresta.Click += new System.EventHandler(this.btnresta_Click);
+            // 
+            // btmmultiplicacion
+            // 
+            this.btmmultiplicacion.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btmmultiplicacion.FlatAppearance.BorderSize = 0;
+            this.btmmultiplicacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btmmultiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btmmultiplicacion.Location = new System.Drawing.Point(3, 71);
+            this.btmmultiplicacion.Name = "btmmultiplicacion";
+            this.btmmultiplicacion.Size = new System.Drawing.Size(76, 62);
+            this.btmmultiplicacion.TabIndex = 0;
+            this.btmmultiplicacion.Text = "*";
+            this.btmmultiplicacion.UseVisualStyleBackColor = false;
+            this.btmmultiplicacion.Click += new System.EventHandler(this.btmmultiplicacion_Click);
+            // 
+            // btndivision
+            // 
+            this.btndivision.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btndivision.FlatAppearance.BorderSize = 0;
+            this.btndivision.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndivision.Location = new System.Drawing.Point(85, 71);
+            this.btndivision.Name = "btndivision";
+            this.btndivision.Size = new System.Drawing.Size(76, 62);
+            this.btndivision.TabIndex = 0;
+            this.btndivision.Text = "/";
+            this.btndivision.UseVisualStyleBackColor = false;
+            this.btndivision.Click += new System.EventHandler(this.btndivision_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnsuma);
+            this.flowLayoutPanel1.Controls.Add(this.btnresta);
+            this.flowLayoutPanel1.Controls.Add(this.btn2);
+            this.flowLayoutPanel1.Controls.Add(this.btmmultiplicacion);
+            this.flowLayoutPanel1.Controls.Add(this.btndivision);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(457, 78);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(306, 218);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // Suma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.Respuesta);
+            this.Controls.Add(this.secondnumber);
+            this.Controls.Add(this.firstnumber);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.Number2);
+            this.Controls.Add(this.Number1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Suma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Suma de Dos Números";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnsuma;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label Number1;
+        private System.Windows.Forms.Label Number2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox firstnumber;
+        private System.Windows.Forms.TextBox secondnumber;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.TextBox Respuesta;
+        private System.Windows.Forms.Button btnresta;
+        private System.Windows.Forms.Button btmmultiplicacion;
+        private System.Windows.Forms.Button btndivision;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Suma/Suma/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suma
+{
+    public partial class Suma : Form
+    {
+        public Suma()
+        {
+            InitializeComponent();
+        }
+
+        double num1 = 0.0;
+        double num2 = 0.0;
+        double operacion = 0.0;
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            firstnumber.Clear();
+            secondnumber.Clear();
+            Respuesta.Clear();
+        }
+
+        public void Asignacion() 
+        {
+            firstnumber.Text = firstnumber.Text.Replace(".", ",");
+            secondnumber.Text = secondnumber.Text.Replace(".", ",");
+            num1 = Convert.ToDouble(firstnumber.Text);
+            num2 = Convert.ToDouble(secondnumber.Text);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Sumatoria();
+        }
+
+        public void Sumatoria() 
+        {
+            try
+            {
+                Asignacion();
+                operacion = num1 + num2;
+
+                Respuesta.Text = operacion.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnresta_Click(object sender, EventArgs e)
+        {
+            resta();
+        }
+
+        public void resta()
+        {
+            try
+            {
+                Asignacion();
+
+                operacion = num1 - num2;
+
+                Respuesta.Text = operacion.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btmmultiplicacion_Click(object sender, EventArgs e)
+        {
+            multiplicacion();
+        }
+
+        public void multiplicacion()
+        {
+            try
+            {
+                Asignacion();
+
+                operacion = num1 * num2;
+
+                Respuesta.Text = operacion.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btndivision_Click(object sender, EventArgs e)
+        {
+            Division();
+        }
+
+        public void Division()
+        {
+            try
+            {
+                Asignacion();
+
+                operacion = num1 / num2;
+
+                Respuesta.Text = operacion.ToString();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
